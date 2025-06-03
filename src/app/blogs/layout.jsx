f1e65080aa0cff31f03d2acc6faf71f75935e477
rasoolabdi <M0rd00r@yahoo.com>
2025-06-03 01:59:38 -0700

    get all category

--- src/app/blogs/layout.jsx
+++ src/app/blogs/layout.jsx
@@ -1,3 +1,4 @@
+import CategoryList from "./_components/CategoryList";
 
 export const metadata = {
     title: "بلاگ ها"
@@ -9,7 +10,7 @@ function Layout({ children }) {
             <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
             <div className="grid grid-cols-12 gap-8">
                 <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
-                    category list
+                    <CategoryList />
                 </div>
                 <div className="col-span-12 lg:col-span-8 xl:col-span-9">
                     { children }
