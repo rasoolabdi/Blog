d9d2340dcbe42cb1a39e16d0d5f032305c36eb85
rasoolabdi <M0rd00r@yahoo.com>
2025-06-08 08:07:13 -0700

    added single page post

--- src/app/blogs/(PostList)/page.jsx
+++ src/app/blogs/(PostList)/page.jsx
@@ -1,12 +1,11 @@
 import { Suspense } from "react";
-import PostList from "./_components/PostList";
 import Spinner from "@/ui/Spinner";
+import PostList from "../_components/PostList";
 
 async function BlogPage() {   
     return (
         <div>
             <p>لیست پست ها</p>
-
             <Suspense fallback={<Spinner />}>
                 <PostList />
             </Suspense>
